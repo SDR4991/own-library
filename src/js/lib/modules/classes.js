a8d8361d63dde3b263fd95a25d95dc88fad5808e
Oleksandr <sidrsidr2@gmail.com>
2024-03-21 20:32:27 +0200

    accordion & tabs

--- src/js/lib/modules/classes.js
+++ src/js/lib/modules/classes.js
@@ -1,12 +1,12 @@
 import $ from '../core';
 
 $.prototype.addClass = function(...classNames){
-    for(let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
         this[i].classList.add(...classNames);
     }
 
     return this;
-}
+};
 
 $.prototype.removeClass = function(...classNames){
     for(let i = 0; i < this.length; i++){
